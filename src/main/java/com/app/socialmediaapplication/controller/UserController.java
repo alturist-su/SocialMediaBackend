34cbb7340e53e9fbb29c656194f902d548b02987
alturist-su <aimsushmitasingh@gmail.com>
2024-05-04 23:21:13 +0530

    User modal

---
+++ src/main/java/com/app/socialmediaapplication/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.app.socialmediaapplication.controller;
+
+import com.app.socialmediaapplication.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @GetMapping
+    public List<User> getUsers(){
+        List<User> users = new ArrayList<>();
+        User user1 = new User("John","K","john@email","johnpass");
+
+        users.add(user1);
+
+        return users;
+    }
+
+    @GetMapping("/url")
+    public String getMappingWithUrl(){
+        return "With url";
+    }
+}
