756ed362db208bf887b5226733186cb8e5657247
Sushmita Singh <83809957+alturist-su@users.noreply.github.com>
2024-06-16 02:07:31 +0530

    Update UserController.java

--- src/main/java/com/app/socialmediaapplication/controller/UserController.java
+++ src/main/java/com/app/socialmediaapplication/controller/UserController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 
-//
-//User controller class
+
 @RestController
 public class UserController {
 
