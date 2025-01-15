8cab817e744d9a6b13fde1e72fde90840621c872
susingh <aimsushmitasingh@gmail.com>
2025-01-16 00:04:55 +0530

    Reverted jwt service

--- src/main/java/com/app/socialmediaapplication/controller/AuthController.java
+++ src/main/java/com/app/socialmediaapplication/controller/AuthController.java
@@ -1,10 +1,8 @@
-// src/main/java/com/app/socialmediaapplication/controller/AuthController.java
 package com.app.socialmediaapplication.controller;
 
 import com.app.socialmediaapplication.exception.UserException;
 import com.app.socialmediaapplication.model.User;
 import com.app.socialmediaapplication.repository.UserRepository;
-import com.app.socialmediaapplication.service.JwtTokenGeneratorService;
 import com.app.socialmediaapplication.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -27,21 +25,17 @@ public class AuthController {
     @Autowired
     private UserRepository userRepository;
 
-    @Autowired
-    private JwtTokenGeneratorService jwtTokenGeneratorService;
-
     @PostMapping("/register")
-    public ResponseEntity<String> registerUserHandler(@RequestBody User user) throws UserException {
+    public ResponseEntity<User> registerUserHandler(@RequestBody User user) throws UserException{
         User createdUser = userService.registerUser(user);
-        String token = jwtTokenGeneratorService.generateToken(createdUser.getUsername());
-        return new ResponseEntity<>(token, HttpStatus.OK);
+        return new ResponseEntity<>(createdUser, HttpStatus.OK);
     }
 
     @GetMapping("/signin")
-    public ResponseEntity<User> signinHandler(Authentication auth) throws BadCredentialsException {
+    public ResponseEntity<User> signinHandler(Authentication auth) throws BadCredentialsException{
         Optional<User> opt = userRepository.findByEmail(auth.getName());
-        if (opt.isPresent()) {
-            return new ResponseEntity<>(opt.get(), HttpStatus.ACCEPTED);
+        if(opt.isPresent()){
+            return new ResponseEntity<User>(opt.get(),HttpStatus.ACCEPTED);
         }
 
         throw new BadCredentialsException("Invalid Username or Password");
